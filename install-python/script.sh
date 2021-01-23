470ce9cb03853cff205c8d9da804e6b77d5571a4
Nathan Archer <77569421+pheianox@users.noreply.github.com>
2021-01-23 04:22:02 +0400

    Update script.sh

--- install-python/script.sh
+++ install-python/script.sh
@@ -1,9 +1,16 @@
 #! /bin/bash
-echo -n "Which version of Python do you want to install? :"
-read version
+echo "[#] Python Installation"
+echo -n "Diring Python installation your system will be updated. Do you want to continue? (y, n)"
+read res
+If (( $res -eq "y" )) 
+do
+  echo -n "Which version of Python do you want to install? :"
+  read version
 
-sudo apt update
-sudo apt install software-properties-common
-sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt update
-sudo apt install python$version && echo "Python${version} installed"
+  sudo apt update
+  sudo apt install
+  software-properties-common
+  sudo add-apt-repository ppa:deadsnakes/ppa
+  sudo apt update
+  sudo apt install python$version && echo "Python${version} installed"
+  
